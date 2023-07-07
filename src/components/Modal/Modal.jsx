731356a3b39d00db701d71ed57702a41798dd99c
jonghwa4077 <ehdxka3@naver.com>
2023-07-07 09:20:30 +0900

    Inter: 1번 모달 인터페이스 작성

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import * as S from ".ModalStyle/Modal"
+import React, {useState} from 'react'
+import * as S from "./ModalStyle"
 
 const Modal = () => {
+  const [firstModal, setFisrtModal] = useState(false)
+
+  const onFirstModal = () => {
+    setFisrtModal(!firstModal)
+  }
+
   return (
     <div>
         <h1>Modal</h1>
+        <button onClick={onFirstModal}>first modal</button>
+        <button>second modal</button>
+        {firstModal && (
+          <div>
+            <div></div>
+            <div>
+              <h4>리액트를 배웁시다</h4>
+              <button>닫기</button>
+              <button>확인</button>
+            </div>
+        </div>
+        )}
     </div>
   )
 }
