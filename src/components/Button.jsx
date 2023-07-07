2f42e9e35a8a5438d873d5f92c232f25c7568bde
leolego03 <leolego03@gmail.com>
2023-07-07 17:32:47 +0900

    Feat: Button, Input 기능 구현

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
-import * as S from "../style/Appstyle"
+// import React from 'react'
+// import * as S from "../style/Appstyle"
+import "../App.css";
 
 const Button = () => {
+  const onClickPrimaryButton = () => {
+    alert("Primary Button Clicked!!")
+  };
+
+  const onClickNevigateButton = () => {
+    alert(prompt("Nevigate Button Clicked!!"));
+  };
+
   return (
-    <div>
+    <>
+      <div>
         <h1>Button</h1>
-    </div>
+      </div>
+    
+      <div className="Button-container">
+        <button
+          onClick={onClickPrimaryButton}
+        >
+          Large Primary Button
+        </button>
+
+        <button>Medium</button>
+        <button>small</button>
+      </div>
+
+      <div className="Button-container">
+        <button
+          onClick={onClickNevigateButton}
+        >
+          Large Nevigate Button
+        </button>
+
+        <button>Medium</button>
+        <button>small</button>
+      </div>
+    </>
   )
 }
 
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
-import * as S from "../style/Appstyle"
+// import React from 'react'
+// import * as S from "../style/Appstyle"
+import { useState } from "react";
+import "../App.css";
 
 const Input = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("0");
+
+  const onChangeName = (event) => {
+    setName(event.target.value);
+  }
+
+  const onChangePrice = (event) => {
+    let value = event.target.value;
+    value = Number(value.replaceAll(",", ""));
+    if(isNaN(value)) {
+      setPrice("0");
+    } else {
+      const formatValue = value.toLocaleString("ko-KR");
+      setPrice(formatValue);
+    }
+  }
+
+  const onSubmitForm = (event) => {
+    event.preventDefault();
+    if (name === "" || price === "0") {
+      alert("이름과 가격을 모두 입력해주세요.")
+      return;
+    }
+
+    alert(`{ name: ${name}, price: ${price.replaceAll(",", "")}} }`);
+  }
+
   return (
-    <div>
+    <>
+      <div>
         <h1>Input</h1>
-    </div>
+      </div>
+
+      <div className="Form-container">
+        <form onSubmit={onSubmitForm}>
+          <label>이름</label>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={onChangeName}
+          ></input>
+
+          <label>가격</label>
+          <input
+            type="text"
+            name="price"
+            value={price}
+            onChange={onChangePrice}
+          ></input>
+
+          <button
+            type="submit"
+          >
+            저장
+          </button>
+        </form>
+      </div>
+    </>
   )
 }
 
