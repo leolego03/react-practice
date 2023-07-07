3c8ee663bf696f63afcc0c11512e43c43aa9dcfa
jonghwa4077 <ehdxka3@naver.com>
2023-07-07 10:11:38 +0900

    Inter: select 인터페이스 작업

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Button from './components/Button'
 import Input from './components/Input'
 import Modal from './components/Modal/Modal'
-import Select from './components/Select'
+import Select from './components/Select/Select'
 const App = () => {
   return (
     <div>
---
+++ src/components/Select/Select.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import * as S from "./SelectStyle"
+
+const Select = () => {
+  return (
+    <div>
+        <h1>Select</h1>
+        <div>
+          <select>
+            <option>스프링</option>
+            <option>자바</option>
+            <option>리액트</option>
+            <option>노드</option>
+          </select>
+          <select>
+            <option>스프링</option>
+            <option>자바</option>
+            <option>리액트</option>
+            <option>노드</option>
+          </select>
+        </div>
+    </div>
+  )
+}
+
+export default Select
